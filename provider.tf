42bd4a8e2b5d1a56344fe0cb904faf954cb03962
Ravindra Kumar <thecodecloud.in@gmail.com>
2025-02-17 10:29:02 +0530

    updated gcp resource

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@
 
 provider "google" {
   #credentials = file("/Users/ravindrakumar/Downloads/my-gcp-project-451114-d57b17f6bdaf.json")
+  credentials = "./gcs-secret.json"
   project     = "my-gcp-project-451114"
   region      = "us-central1"
 }
